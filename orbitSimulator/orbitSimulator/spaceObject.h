b7ab6ceb853509b6e712acad8aa070b41d6d0eb3
ajrobbins04 <ajrobbins04@gmail.com>
2023-06-16 17:38:49 -0700

    ready to work on Satellite class. compiles

--- orbitSimulator/orbitSimulator/spaceObject.h
+++ orbitSimulator/orbitSimulator/spaceObject.h
@@ -29,15 +29,22 @@ public:
 		initialize(name, pos, radius, direction);
 	}
 	
-	void initialize(string name, Position pos, double radius, Direction direction)
+	void initialize(string name, Position pos, double radiusPixels, Direction direction)
 	{
 		assert(name != " " || name != "");
 		setName(name);
 		
-		// Radius given in pixels. Must convert to meters.
-		assert(radius > 0);
-		double radiusMeters = pos.convertToMeters(radius);
-		setRadius(radiusMeters);
+		
+		// Earth's radius = 6378000 meters
+		// All other radius values are given in pixels, and must be converted to meters
+		if (radius != 6378000)
+		{
+			// fragments have smallest radius of 2 px
+			// GPS Satellite has largest radius of 12 px
+			assert(radius >= 2 && radius <= 12);
+			double radiusMeters = pos.convertToMeters(radius);
+			setRadius(radiusMeters);
+		}
 	}
 	
 	void setName(string name)     { this->name = name;     }
